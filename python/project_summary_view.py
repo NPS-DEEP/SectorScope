b728835c6305cc70d7b1b3e34a79d169de262c31
Bruce Allen <bdallen@nps.edu>
2015-10-09 16:38:10 -0700

    add summary information

--- python/project_summary_view.py
+++ python/project_summary_view.py
@@ -28,6 +28,10 @@ class ProjectSummaryView():
         tkinter.Label(self.frame, text="Project", bg=colors.BACKGROUND).pack(
                                              side=tkinter.TOP, pady=(0,4))
 
+        # scan path
+        self._scan_path_text = tkinter.Label(self.frame, bg=colors.BACKGROUND)
+        self._scan_path_text.pack(side=tkinter.TOP, anchor="w")
+
         # media image
         self._image_text = tkinter.Label(self.frame, bg=colors.BACKGROUND)
         self._image_text.pack(side=tkinter.TOP, anchor="w")
@@ -40,6 +44,10 @@ class ProjectSummaryView():
         self._database_text = tkinter.Label(self.frame, bg=colors.BACKGROUND)
         self._database_text.pack(side=tkinter.TOP, anchor="w")
 
+        # size statistics
+        self._sizes_text = tkinter.Label(self.frame, bg=colors.BACKGROUND)
+        self._sizes_text.pack(side=tkinter.TOP, anchor="w")
+
         # register to receive identified_data change events
         identified_data.set_callback(self._handle_identified_data_change)
 
@@ -50,15 +58,25 @@ class ProjectSummaryView():
     def _handle_identified_data_change(self, *args):
         if self._identified_data.image_filename:
             # identified_data opened
+            self._scan_path_text["text"] = 'Scan path: %s' % \
+                               self._identified_data.be_dir
             self._image_text["text"] = 'Image: %s' % \
                                self._identified_data.image_filename
             self._image_size_text["text"] = 'Image size: %s' % \
                                offset_string(self._identified_data.image_size)
             self._database_text["text"] = 'Database: %s' % \
                                self._identified_data.hashdb_dir
+            self._sizes_text["text"] = 'Matches: Paths: %s        ' \
+                                  'Hashes: %s        Sources: %s' % (
+                             len(self._identified_data.forensic_paths),
+                             len(self._identified_data.hashes),
+                             len(self._identified_data.source_details))
+
         else:
             # identified_data not opened
+            self._scan_path_text["text"] = 'Scan path: Not opened'
             self._image_text["text"] = 'Image: Not opened'
             self._image_size_text["text"] = 'Image size: Not opened'
             self._database_text["text"] = 'Database: Not opened'
+            self._database_text["text"] = 'Matches: Not opened'
 
