a8078f7855645f1ff60d1105bb4b6db120dc3ed1
Bruce Allen <bdallen@nps.edu>
2015-11-19 16:45:10 -0800

    add test for reader

--- src/json_scan_reader.hpp
+++ src/json_scan_reader.hpp
@@ -39,15 +39,21 @@
 #define JSON_SCAN_READER_HPP
 
 // Standard includes
-#include <cstdlib>
-#include <cstdio>
+
+//#include <cstdlib>
+//#include <cstdio>
+#include <cstdint>
 #include <string>
+#include <cstring>
 #include <sstream>
 #include <iostream>
 #include <fstream>
 #include <errno.h>
 #include <cassert>
-#include "feature_line.hpp"
+#include <vector>
+#include <map>
+#include <set>
+#include "document.h"
 
 // ************************************************************
 // forensic_paths_t
@@ -64,6 +70,10 @@ class id_offset_pair_t {
   id_offset_pair_t(uint64_t p_source_id, uint64_t p_file_offset) :
                 source_id(p_source_id), file_offset(p_file_offset) {
   }
+};
+
+typedef std::vector<uint64_t> z_t;
+typedef std::vector<id_offset_pair_t> id_offset_pairs_t;
 
 typedef std::set<uint64_t> source_ids_t;
 
@@ -71,11 +81,15 @@ class hash_attributes_t {
   public:
   uint64_t count;
   source_ids_t source_ids;
+  id_offset_pairs_t id_offset_pairs;
   bool has_label;
   hash_attributes_t(uint64_t p_count, const source_ids_t p_source_ids,
+                    const id_offset_pairs_t& p_id_offset_pairs,
                     bool p_has_label) :
-           count(p_count), source_ids(p_source_ids), has_label(p_has_label) {
+             count(p_count), source_ids(p_source_ids),
+             id_offset_pairs(p_id_offset_pairs), has_label(p_has_label) {
   }
+};
 
 typedef std::map<std::string, hash_attributes_t> hashes_t;
 
@@ -88,16 +102,17 @@ class source_detail_t {
   std::string repository_name;
   std::string filename;
   uint64_t filesize;
-  std::string binary_hash;
+  std::string file_hashdigest;
   source_detail_t(uint64_t p_source_id,
                   const std::string& p_repository_name,
                   const std::string& p_filename,
                   uint64_t p_filesize,
-                  const std::string& p_binary_hash):
+                  const std::string& p_file_hashdigest):
            source_id(p_source_id),
            repository_name(p_repository_name), filename(p_filename),
-           filesize(p_filesize), binary_hash(p_binary_hash) {
+           filesize(p_filesize), file_hashdigest(p_file_hashdigest) {
   }
+};
 
 typedef std::map<uint64_t, source_detail_t> source_details_t;
 
@@ -106,12 +121,18 @@ typedef std::map<uint64_t, source_detail_t> source_details_t;
 // ************************************************************
 
 class json_scan_reader_t {
-  public:
 
+  private:
+  std::ifstream in;
+
+  // do not allow copy or assignment
+  json_scan_reader_t(const json_scan_reader_t&);
+  json_scan_reader_t& operator=(const json_scan_reader_t&);
+
+  public:
   bool is_valid;
   std::string error_string;
-  std::string filename;
-  std::ifstream in;
+  std::string json_scan_filename;
   forensic_paths_t* forensic_paths;
   hashes_t* hashes;
   source_details_t* source_details;
@@ -123,30 +144,33 @@ class json_scan_reader_t {
   }
 
   json_scan_reader_t(std::string p_filename) :
-              is_valid(true)
-              error_string("")
-              filename(p_filename),
-              in(filename.c_str()),
+              in(p_filename.c_str()),
+              is_valid(true),
+              error_string(""),
+              json_scan_filename(p_filename),
               forensic_paths(0),
               hashes(0),
               source_details(0) {
 
     // scan data
-    forensic_paths = new forensic_paths_t()
-    hashes = new hashes_t()
-    source_details = new source_details_t()
+    forensic_paths = new forensic_paths_t();
+    hashes = new hashes_t();
+    source_details = new source_details_t();
 
     // see that the file opened
     if (!in.is_open()) {
       is_valid = false;
       std::stringstream ss;
-      ss << "Cannot open " << filename << ": " << strerror(errno);
-      return
+      ss << "Cannot open " << json_scan_filename << ": " << strerror(errno);
+      error_string = ss.str();
+      return;
     }
 
     // read the JSON scan input and populate the scan data
+    int i = 0;
     std::string line;
     while(getline(in, line)) {
+      i++;
 
       // skip comment lines
       if (line[0] == '#') {
@@ -154,64 +178,107 @@ class json_scan_reader_t {
         continue;
       }
 
-      // parse line of "offset tab hexdigest tab count"
+      // parse line of "offset <tab> hexdigest <tab> source data"
 
       // find tabs
       size_t tab_index1 = line.find('\t');
       if (tab_index1 == std::string::npos) {
+        std::cerr << "invalid line: " << line << "\n";
         continue;
       }
       size_t tab_index2 = line.find('\t', tab_index1 + 1);
       if (tab_index2 == std::string::npos) {
+        std::cerr << "invalid line: " << line << "\n";
         continue;
       }
 
-      // forensic path
+      // parse the forensic path containing the offset
       std::string forensic_path = line.substr(0, tab_index1);
 
-      // feature
-      std::string feature = line.substr(tab_index1+1, tab_index2 - tab_index1 - 1);
+      // parse the feature containing the block_hash
+      std::string block_hash =
+                  line.substr(tab_index1+1, tab_index2 - tab_index1 - 1);
 
-      // context
-      std::string context = line.substr(tab_index2+1);
+      // append <key=offset, value=hash> to forensic_paths
+      uint64_t offset = std::atol(forensic_path.c_str());
+      std::pair<forensic_paths_t::iterator, bool> ret;
+      ret = forensic_paths->insert(std::pair<uint64_t, std::string>(
+                                                      offset, block_hash));
+      if (ret.second == false) {
+        std::cerr << "invalid line, path already read: " << line << "\n";
+        continue;
+      }
 
-      feature_line = feature_line_t(forensic_path, feature, context);
+      // do not reprocess source information associated with a hash
+      if (hashes->find(block_hash) != hashes->end()) {
+        // hash already processed
+        continue;
+      }
 
-      return;
+      // parse context of json data containing source information
+      std::string json_data = line.substr(tab_index2+1);
+
+std::cout << "Starting JSON data: '" << json_data << "'\n";
+
+      // create a source ID set for this hash
+      source_ids_t source_ids;                // set
+      id_offset_pairs_t id_offset_pairs;      // vector
+
+      // parse json data into a JSON DOM document
+      rapidjson::Document document;
+      if (document.Parse(json_data.c_str()).HasParseError()) {
+        is_valid = false;
+        error_string = "Invalid JSON syntax.";
+        return;
+      }
+      if (!document.IsObject()) {
+        is_valid = false;
+        error_string = "Invalid JSON object.";
+        return;
+      }
+      assert(document.IsObject()); //zz ease this.
+
+      const rapidjson::Value& source_part = document[1];
+      const rapidjson::Value& source_list = source_part["sources"];
+
+      // read source_ids and id_offset_pairs
+      for (rapidjson::Value::ConstValueIterator it =
+                              source_list.Begin();
+                              it != source_list.End(); ++it) {
+        const rapidjson::Value& source_item = *it;
+
+        const uint64_t source_id = source_item["source_id"].GetUint64();
+
+        // add source_id to source_ids set
+        source_ids.insert(source_id);
+
+        // add pair to pairs list
+        uint64_t file_offset = source_item["file_offset"].GetUint64();
+        id_offset_pairs.push_back(id_offset_pair_t(source_id, file_offset));
+
+        // also store source details if first time seen
+        if (source_list.HasMember("filename")) {
+          std::string filename = source_list["filename"].GetString();
+          std::string repository_name =
+                                   source_list["repository_name"].GetString();
+          uint64_t filesize = source_list["filesize"].GetUint64();
+          std::string file_hashdigest =
+                                   source_list["file_hashdigest"].GetString();
+          source_details->insert(std::pair<uint64_t, source_detail_t>(
+                    source_id, source_detail_t(source_id,
+                                               repository_name, filename,
+                                               filesize, file_hashdigest)));
+        }
+      }
+
+      // get any hash entropy label.  For now, get this from source[0].
+      bool has_label = source_list[0].HasMember("label");
+
+      // store hash and its attributes in hashes_t
+      uint64_t count = source_ids.size();
+      hashes->insert(std::pair<std::string, hash_attributes_t>(block_hash,
+           hash_attributes_t(count, source_ids, id_offset_pairs, has_label)));
     }
-
-    // at eof
-    at_end = true;
-  }
-
-  public:
-  feature_scan_reader_t(std::string p_filename) :
-              filename(p_filename),
-              in(filename.c_str()) {
-
-    // see that the file opened
-    if (!in.is_open()) {
-      std::cout << "Cannot open " << filename << ": " << strerror(errno) << "\n";
-      exit(1);
-    }
-
-    // read the first feature
-    read_feature();
-  }
-
-  feature_line_t read() {
-    if (at_end) {
-      // program error
-      assert(0);
-    }
-    feature_line_t temp = feature_line;
-    read_feature();
-    return temp;
-  }
-
-  // at EOF
-  bool at_eof() {
-    return at_end;
   }
 };
 
---
+++ src/test.cpp
@@ -0,0 +1,33 @@
+#include <config.h>
+#include "json_scan_reader.hpp"
+
+//#include <cstdlib>
+//#include <cstdio>
+//#include <string>
+//#include <sstream>
+#include <iostream>
+//#include <algorithm>
+//#include <vector>
+//#include <getopt.h>
+
+int main(int argc,char **argv) {
+  std::cout << "test " << PACKAGE_VERSION << "\n";
+
+/*
+  // read data
+  json_scan_reader_t json_scan_reader("/home/bdallen/Kitty/be_kitty_out/identified_blocks_expanded.txt")
+
+  // show something
+  std::cout << "is_valid=" << json_scan_reader.is_valid
+            << ", error_string=" << json_scan_reader.error_string
+            << ", json_scan_filename=" << json_scan_reader.json_scan_filename
+            << ", len forensic_paths="
+            << json_scan_reader->forensic_paths.size()
+            << ", len hashes=" << json_scan_reader->hashes.size()
+            << ", len source_details="
+            << json_scan_reader->source_details.size()
+            ;
+*/
+  return 0;
+}
+
